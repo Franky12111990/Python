0496b5d953c6512b2e424e03537b0a65dc79c158
Franky12111990 <121640886+Franky12111990@users.noreply.github.com>
2024-03-18 11:30:36 +0300

    magical method  Наследование.py

--- Наследование.py
+++ Наследование.py
@@ -4,24 +4,18 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+    def __eq__(self, other):
+        return self.name == other.name and self.surname == other.surname
 
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.courses_attached = []
+    def __lt__(self, other):
+        if self.surname == other.surname:
+            return self.name < other.name
+        return self.surname < other.surname
 
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.courses_attached = []
+    def __gt__(self, other):
+        if self.surname == other.surname:
+            return self.name > other.name
+        return self.surname > other.surname
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -29,6 +23,5 @@ class Reviewer(Mentor):
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
-            return 'Оценка добавлена'
         else:
             return 'Ошибка'
